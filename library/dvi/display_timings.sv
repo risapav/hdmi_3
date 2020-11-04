f5a4d3019605eee92411cc1faaca97ec3ed8683a
risapav <risapav@gmail.com>
2020-11-04 08:49:06 +0100

    Update display_timings.sv
    added constants

--- library/dvi/display_timings.sv
+++ library/dvi/display_timings.sv
@@ -48,26 +48,42 @@
 `timescale 1ns / 1ps
 
 module display_timings (
-    input  wire logic clk_pix,   // pixel clock
-    input  wire logic rst,       // reset
-    output      logic [9:0] sx,  // horizontal screen position
-    output      logic [9:0] sy,  // vertical screen position
-    output      logic hsync,     // horizontal sync
-    output      logic vsync,     // vertical sync
-    output      logic de         // data enable (low in blanking interval)
-    );
-
+	input  wire logic clk_pix,   // pixel clock
+	input  wire logic rst,       // reset
+	output      logic [9:0] sx,  // horizontal screen position
+	output      logic [9:0] sy,  // vertical screen position
+	output      logic hsync,     // horizontal sync
+	output      logic vsync,     // vertical sync
+	output      logic de         // data enable (low in blanking interval)
+	);
+	
+	// size of screen (including blanking)
+	parameter H_RES = 800;
+	parameter V_RES = 525;
+	// size of visible screen
+	parameter X_RES = 640;	//Active Pixels 
+	parameter Y_RES = 480;	//Active Lines
+	
 	// horizontal timings
-	parameter HA_END = 639;          // end of active pixels
-	parameter HS_STA = HA_END + 16;  // sync starts after front porch
-	parameter HS_END = HS_STA + 96;  // sync ends
-	parameter LINE   = 799;          // last pixel on line (after back porch)
+	parameter H_FRONT_PORCH	16;	// Front Porch
+	parameter H_SYNC_WIDTH 96;	// Sync Width
+	parameter H_BACK_PORCH 48;	// Back Porch
+	
+	parameter HA_END = X_RES - 1;	// end of active pixels
+	parameter HS_STA = HA_END + H_FRONT_PORCH;	// sync starts after front porch
+	parameter HS_END = HS_STA + H_SYNC_WIDTH;	// sync ends
+	parameter LINE   = H_RES - 1;	// last pixel on line (after back porch)
 
 	// vertical timings
-	parameter VA_END = 479;          // end of active pixels
-	parameter VS_STA = VA_END + 10;  // sync starts after front porch
-	parameter VS_END = VS_STA + 2;   // sync ends
-	parameter SCREEN = 524;          // last line on screen (after back porch)
+	parameter V_FRONT_PORCH	10;	// Front Porch
+	parameter V_SYNC_WIDTH 2;	// Sync Width
+	parameter V_BACK_PORCH 33;	// Back Porch
+
+	parameter VA_END = Y_RES - 1;	// end of active pixels
+	parameter VS_STA = VA_END + V_FRONT_PORCH;	// sync starts after front porch
+	
+	parameter VS_END = VS_STA + V_SYNC_WIDTH;	// sync ends
+	parameter SCREEN = V_RES - 1;	// last line on screen (after back porch)
 
 	always_comb 
 		begin
