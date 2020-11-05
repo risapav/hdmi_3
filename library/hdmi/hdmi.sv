32ea31f59280795b3f4175512175cd893383a326
risapav <risapav@gmail.com>
2020-11-05 08:43:11 +0100

    Update hdmi.sv

--- library/hdmi/hdmi.sv
+++ library/hdmi/hdmi.sv
@@ -19,7 +19,7 @@ module hdmi
 	parameter bit DVI_OUTPUT = 1'b0,
 
 	// When enabled, DDRIO (Double Data Rate I/O) is used and clk_pixel_x10 only needs to be five times as fast as clk_pixel.
-	parameter bit DDRIO = 1'b0,
+//	parameter bit DDRIO = 1'b0,
 
 	// **All parameters below matter ONLY IF you plan on sending auxiliary data (DVI_OUTPUT == 1'b0)**
 
@@ -44,7 +44,7 @@ module hdmi
 	input logic clk_pixel_x10,
 	input logic clk_pixel,
 	input logic clk_audio,
-	input logic [23:0] rgb,
+	input logic [23:0] rgb, // vstup pre video data
 	input logic [AUDIO_BIT_WIDTH-1:0] audio_sample_word [1:0],
 
 	// These outputs go to your HDMI port
@@ -95,51 +95,6 @@ module hdmi
 				assign hsync = ~(cx >= 16 && cx < 16 + 62);
 				assign vsync = ~(cy >= 9 && cy < 9 + 6);
 				end
-			4:
-			begin
-				assign frame_width = 1650;
-				assign frame_height = 750;
-				assign screen_width = 1280;
-				assign screen_height = 720;
-				assign hsync = cx >= 110 && cx < 110 + 40;
-				assign vsync = cy >= 5 && cy < 5 + 5;
-			end
-			16:
-			begin
-				assign frame_width = 2200;
-				assign frame_height = 1125;
-				assign screen_width = 1920;
-				assign screen_height = 1080;
-				assign hsync = cx >= 88 && cx < 88 + 44;
-				assign vsync = cy >= 4 && cy < 4 + 5;
-			end
-			17, 18:
-			begin
-				assign frame_width = 864;
-				assign frame_height = 625;
-				assign screen_width = 720;
-				assign screen_height = 576;
-				assign hsync = ~(cx >= 12 && cx < 12 + 64);
-				assign vsync = ~(cy >= 5 && cy < 5 + 5);
-			end
-			19:
-			begin
-				assign frame_width = 1980;
-				assign frame_height = 750;
-				assign screen_width = 1280;
-				assign screen_height = 720;
-				assign hsync = cx >= 440 && cx < 440 + 40;
-				assign vsync = cy >= 5 && cy < 5 + 5;
-			end
-			97, 107:
-			begin
-				assign frame_width = 4400;
-				assign frame_height = 2250;
-				assign screen_width = 3840;
-				assign screen_height = 2160;
-				assign hsync = cx >= 176 && cx < 176 + 88;
-				assign vsync = cy >= 8 && cy < 8 + 10;
-			end
 		endcase
 		assign screen_start_x = frame_width - screen_width;
 		assign screen_start_y = frame_height - screen_height;
@@ -207,21 +162,21 @@ module hdmi
 				assign video_field_end = cx == frame_width - 1'b1 && cy == frame_height - 1'b1;
 				logic [4:0] packet_pixel_counter;
 				localparam real VIDEO_RATE = (VIDEO_ID_CODE == 1 ? 25.2E6
-							: VIDEO_ID_CODE == 2 || VIDEO_ID_CODE == 3 ? 27.027E6
-							: VIDEO_ID_CODE == 4 ? 74.25E6
-							: VIDEO_ID_CODE == 16 ? 148.5E6
-							: VIDEO_ID_CODE == 17 || VIDEO_ID_CODE == 18 ? 27E6
-							: VIDEO_ID_CODE == 19 ? 74.25E6
-							: VIDEO_ID_CODE == 97 || VIDEO_ID_CODE == 107 ? 594E6
-							: 0) * (VIDEO_REFRESH_RATE == 59.94 ? 1000.0/1001.0 : 1); // https://groups.google.com/forum/#!topic/sci.engr.advanced-tv/DQcGk5R_zsM
+					: VIDEO_ID_CODE == 2 || VIDEO_ID_CODE == 3 ? 27.027E6
+					: VIDEO_ID_CODE == 4 ? 74.25E6
+					: VIDEO_ID_CODE == 16 ? 148.5E6
+					: VIDEO_ID_CODE == 17 || VIDEO_ID_CODE == 18 ? 27E6
+					: VIDEO_ID_CODE == 19 ? 74.25E6
+					: VIDEO_ID_CODE == 97 || VIDEO_ID_CODE == 107 ? 594E6
+					: 0) * (VIDEO_REFRESH_RATE == 59.94 ? 1000.0/1001.0 : 1); // https://groups.google.com/forum/#!topic/sci.engr.advanced-tv/DQcGk5R_zsM
 				packet_picker #(
-							.VIDEO_ID_CODE(VIDEO_ID_CODE),
-							.VIDEO_RATE(VIDEO_RATE),
-							.AUDIO_RATE(AUDIO_RATE),
-							.AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH),
-							.VENDOR_NAME(VENDOR_NAME),
-							.PRODUCT_DESCRIPTION(PRODUCT_DESCRIPTION),
-							.SOURCE_DEVICE_INFORMATION(SOURCE_DEVICE_INFORMATION)
+					.VIDEO_ID_CODE(VIDEO_ID_CODE),
+					.VIDEO_RATE(VIDEO_RATE),
+					.AUDIO_RATE(AUDIO_RATE),
+					.AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH),
+					.VENDOR_NAME(VENDOR_NAME),
+					.PRODUCT_DESCRIPTION(PRODUCT_DESCRIPTION),
+					.SOURCE_DEVICE_INFORMATION(SOURCE_DEVICE_INFORMATION)
 				) packet_picker (
 					.clk_pixel(clk_pixel), 
 					.clk_audio(clk_audio), 
@@ -318,76 +273,10 @@ module hdmi
 		logic [NUM_CHANNELS-1:0] tmds_current;
 		logic tmds_current_clk;
 
-		if (DDRIO)
-			begin
-				`ifdef SYNTHESIS // TODO: Is this really Vivado? https://forums.xilinx.com/t5/Simulation-and-Verification/Predefined-constant-for-simulation/td-p/986901
-					`ifndef ALTERA_RESERVED_QIS
-						for (i = 0; i < NUM_CHANNELS; i++)
-							begin: oddr2_gen
-								ODDR2 #(.DDR_ALIGNMENT("NONE"), .INIT(1'b0), .SRTYPE("SYNC")) 
-								clock_forward_inst (
-									.Q(tmds_current[i]), 
-									.C0(clk_pixel_x10), 
-									.C1(!clk_pixel_x10), 
-									.CE(1'b1), 
-									.D0(tmds_shift[i][0]), 
-									.D1(tmds_shift[i][1]), 
-									.R(1'b0), 
-									.S(1'b0)
-								);
-							end
-								ODDR2 #(.DDR_ALIGNMENT("NONE"), .INIT(1'b0), .SRTYPE("SYNC")) 
-								clock_forward_inst (
-									.Q(tmds_current_clk), 
-									.C0(clk_pixel_x10), 
-									.C1(!clk_pixel_x10), 
-									.CE(1'b1), 
-									.D0(tmds_shift_clk_pixel[0]), 
-									.D1(tmds_shift_clk_pixel[1]), 
-									.R(1'b0), 
-									.S(1'b0)
-								);
-					`endif
-				`else
-					altDDIO_out DDRIO (
-						.dataout({tmds_current, tmds_current_clk}), 
-						.outclock(clk_pixel_x10), 
-						.datain_h({tmds_shift[2][0], tmds_shift[1][0], tmds_shift[0][0], tmds_shift_clk_pixel[0]}), 
-						.datain_l({tmds_shift[2][1], tmds_shift[1][1], tmds_shift[0][1], tmds_shift_clk_pixel[1]}), 
-						.aclr(1'b0), 
-						.aset(1'b0), 
-						.outclocken(1'b1), 
-						.sclr(1'b0), 
-						.sset(1'b0)
-					);
-					defparam DDRIO.inverted_input_clocks = "OFF", DDRIO.lpm_hint = "UNUSED", DDRIO.lpm_type = "altDDIO_out", DDRIO.power_up_high = "OFF", DDRIO.width = NUM_CHANNELS + 1;
-				`endif
-			end
-		else
-			begin
-				assign tmds_current = {tmds_shift[2][0], tmds_shift[1][0], tmds_shift[0][0]};
-				assign tmds_current_clk = tmds_shift_clk_pixel[0];
-			end
+		//vysledok kodovania, pripravený na export
+		assign tmds_current = {tmds_shift[2][0], tmds_shift[1][0], tmds_shift[0][0]};
+		assign tmds_current_clk = tmds_shift_clk_pixel[0];
 
-`ifndef VERILATOR
-	// Differential signal output
-	`ifdef SYNTHESIS // TODO: Is this really Vivado? https://forums.xilinx.com/t5/Simulation-and-Verification/Predefined-constant-for-simulation/td-p/986901
-		`ifndef ALTERA_RESERVED_QIS
-			for (i = 0; i < NUM_CHANNELS; i++)
-				begin: obufds_gen
-					OBUFDS obufds (
-						.I(tmds_current[i]), 
-						.O(tmds_p[i]), 
-						.OB(tmds_n[i])
-					);
-				end
-			OBUFDS OBUFDS_clock(
-				.I(tmds_current_clk), 
-				.O(tmds_clock_p), 
-				.OB(tmds_clock_n)
-			);
-		`endif
-	`else
 		// If Altera synthesis, a true differential buffer is built with altera_gpio_lite from the Intel IP Catalog.
 		// If simulation, a mocked signal inversion is used.
 		OBUFDS obufds(
@@ -395,8 +284,6 @@ module hdmi
 			.pad_out({tmds_p, tmds_clock_p}), 
 			.pad_out_b({tmds_n, tmds_clock_n})
 		);
-	`endif
-`endif
 
 	endgenerate
 
